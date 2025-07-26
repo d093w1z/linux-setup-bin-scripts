658910bd582d3912769e78ff8d391d765b177881
d093w1z <d093w1z@protonmail.com>
2025-07-27 00:51:24 +0530

    Polybar launch update

--- polybar.launch.sh
+++ polybar.launch.sh
@@ -1,5 +1,39 @@
 #!/usr/bin/env sh
 
+## https://github.com/polybar/polybar/issues/763#issuecomment-450940924
+#!/bin/bash
+(
+  flock 200
+
+  killall -q polybar
+
+  while pgrep -u $UID -x polybar > /dev/null; do sleep 0.5; done
+
+  outputs=$(xrandr --query | grep " connected" | cut -d" " -f1)
+  tray_output=eDP1
+
+  for m in $outputs; do
+    if [[ $m == "HDMI1" ]]; then
+        tray_output=$m
+    fi
+  done
+
+  for m in $outputs; do
+    export MONITOR=$m
+    export TRAY_POSITION=none
+    if [[ $m == $tray_output ]]; then
+      TRAY_POSITION=right
+    fi
+
+    polybar --reload main </dev/null >/var/tmp/polybar-$m.log 2>&1 200>&- &
+    disown
+  done
+) 200>/var/tmp/polybar-launch.lock
+
+
+
+
+## OLD CONFIG ##
 ## https://github.com/polybar/polybar/issues/763#issuecomment-331604987
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
@@ -9,7 +43,6 @@ else
   polybar --reload &
 fi
 
-## OLD CONFIG ##
 # # Terminate already running bar instances
 # killall -q polybar
 # # Wait until the processes have been shut down
