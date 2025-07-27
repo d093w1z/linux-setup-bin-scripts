b842ed7232a3c1f30fe7114bc453fb415b693bdc
d093w1z <d093w1z@protonmail.com>
2025-07-27 17:17:41 +0530

    polybar script update

--- polybar.launch.sh
+++ polybar.launch.sh
@@ -1,5 +1,6 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
+#!/usr/bin/env sh
 ## https://github.com/polybar/polybar/issues/763#issuecomment-450940924
 #!/bin/bash
 (
@@ -10,11 +11,12 @@
   while pgrep -u $UID -x polybar > /dev/null; do sleep 0.5; done
 
   outputs=$(xrandr --query | grep " connected" | cut -d" " -f1)
-  tray_output=eDP1
+  tray_output=HDMI-1
 
   for m in $outputs; do
-    if [[ $m == "HDMI1" ]]; then
+    if [[ $m == "HDMI-1" ]]; then
         tray_output=$m
+        echo $tray_output $m
     fi
   done
 
@@ -25,7 +27,7 @@
       TRAY_POSITION=right
     fi
 
-    polybar --reload main </dev/null >/var/tmp/polybar-$m.log 2>&1 200>&- &
+    polybar --reload mybar </dev/null >/var/tmp/polybar-$m.log 2>&1 200>&- &
     disown
   done
 ) 200>/var/tmp/polybar-launch.lock
@@ -35,13 +37,13 @@
 
 ## OLD CONFIG ##
 ## https://github.com/polybar/polybar/issues/763#issuecomment-331604987
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload &
-  done
-else
-  polybar --reload &
-fi
+# if type "xrandr"; then
+#   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+#     MONITOR=$m polybar --reload &
+#   done
+# else
+#   polybar --reload &
+# fi
 
 # # Terminate already running bar instances
 # killall -q polybar
